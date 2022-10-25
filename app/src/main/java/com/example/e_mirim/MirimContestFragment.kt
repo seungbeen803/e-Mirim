711127a7a4e16830a8e2b2a6dafdb77cbf231cae
seungbeen <seungbin803@gmail.com>
2022-10-25 20:02:20 +0900

    10월 25일 e-Mirim 프로젝트 공모전 ContestInfo

--- app/src/main/java/com/example/e_mirim/MirimContestFragment.kt
+++ app/src/main/java/com/example/e_mirim/MirimContestFragment.kt
@@ -1,6 +1,25 @@
 package com.example.e_mirim
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 
 class MirimContestFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.mirim_contest_fragment, container, false)
+
+        view.findViewById<Button>(R.id.contest_info).setOnClickListener {
+            var intent = Intent()
+        }
+        return view
+    }
 }
