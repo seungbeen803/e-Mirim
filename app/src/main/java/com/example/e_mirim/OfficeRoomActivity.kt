1cbacf3041fc27b4e302873f43fd51ce1c15b1df
seungbeen <seungbin803@gmail.com>
2022-11-22 20:04:13 +0900

    11월 22일 사진 교체

--- app/src/main/java/com/example/e_mirim/OfficeRoomActivity.kt
+++ app/src/main/java/com/example/e_mirim/OfficeRoomActivity.kt
@@ -18,61 +18,61 @@ class OfficeRoomActivity : AppCompatActivity() {
          val map = mutableMapOf<Int, List<TeacherInfo>>()
         // put으로 값 가져오기
         map.put(1, listOf(
-            TeacherInfo(R.drawable.img, "김현수"),
-            TeacherInfo(R.drawable.img, "윤성웅"),
-            TeacherInfo(R.drawable.img, "이호연"),
-            TeacherInfo(R.drawable.img, "윤선희"),
-            TeacherInfo(R.drawable.img_1_4, "민주리"),
-            TeacherInfo(R.drawable.img, "권지웅"),
-            TeacherInfo(R.drawable.img, "김영철"),
-            TeacherInfo(R.drawable.img, "신혜영"),
-            TeacherInfo(R.drawable.img_1_9, "이철호"),
-            TeacherInfo(R.drawable.img, "임정훈"),
-            TeacherInfo(R.drawable.img, "이철희"),
-            TeacherInfo(R.drawable.img_1_11, "정혜선"),
-            TeacherInfo(R.drawable.img_1_12, "김지훈"),
-            TeacherInfo(R.drawable.img, "손명수"),
-            TeacherInfo(R.drawable.img, "이하얀")
+            TeacherInfo(R.drawable.img1_1, "김현수"),
+            TeacherInfo(R.drawable.img1_2, "윤성웅"),
+            TeacherInfo(R.drawable.img3_6, "이호연"),
+            TeacherInfo(R.drawable.img1_4, "윤선희"),
+            TeacherInfo(R.drawable.img1_5, "민주리"),
+            TeacherInfo(R.drawable.img3_6, "권지웅"),
+            TeacherInfo(R.drawable.img1_7, "김영철"),
+            TeacherInfo(R.drawable.img3_6, "신혜영"),
+            TeacherInfo(R.drawable.img1_9, "이철호"),
+            TeacherInfo(R.drawable.img3_6, "임정훈"),
+            TeacherInfo(R.drawable.img1_11, "이철희"),
+            TeacherInfo(R.drawable.img1_12, "정혜선"),
+            TeacherInfo(R.drawable.img3_6, "김지훈"),
+            TeacherInfo(R.drawable.img3_6, "손명수"),
+            TeacherInfo(R.drawable.img3_6, "이하얀")
         ))
 
         map.put(2, listOf(
-            TeacherInfo(R.drawable.img_2, "박향규"),
-            TeacherInfo(R.drawable.img_2_2, "김명준"),
-            TeacherInfo(R.drawable.img_2_3, "이은주"),
-            TeacherInfo(R.drawable.img_3_7, "이정임"),
-            TeacherInfo(R.drawable.img_2_5, "최규정"),
-            TeacherInfo(R.drawable.img_2_6, "김보경"),
-            TeacherInfo(R.drawable.img_2_7, "고낙은"),
-            TeacherInfo(R.drawable.img_3_7, "김민혜"),
-            TeacherInfo(R.drawable.img_2_9, "박지우")
+            TeacherInfo(R.drawable.img3_6, "박향규"),
+            TeacherInfo(R.drawable.img3_6, "김명준"),
+            TeacherInfo(R.drawable.img3_6, "이은주"),
+            TeacherInfo(R.drawable.img3_6, "이정임"),
+            TeacherInfo(R.drawable.img3_6, "최규정"),
+            TeacherInfo(R.drawable.img2_6, "김보경"),
+            TeacherInfo(R.drawable.img2_7, "고낙은"),
+            TeacherInfo(R.drawable.img3_6, "김민혜"),
+            TeacherInfo(R.drawable.img3_6, "박지우")
             ))
 
         map.put(3, listOf(
-            TeacherInfo(R.drawable.img_3_2, "백현정"),
-            TeacherInfo(R.drawable.img_3_2, "함기훈"),
-            TeacherInfo(R.drawable.img_3_2, "이재민"),
-            TeacherInfo(R.drawable.img_3_7, "이호걸"),
-            TeacherInfo(R.drawable.img_3_5, "박성래"),
-            TeacherInfo(R.drawable.img_3_7, "유병석"),
-            TeacherInfo(R.drawable.img_3_7, "이형섭"),
-            TeacherInfo(R.drawable.img_3_7, "장민주")
+            TeacherInfo(R.drawable.img3_6, "백현정"),
+            TeacherInfo(R.drawable.img3_6, "함기훈"),
+            TeacherInfo(R.drawable.img3_6, "이재민"),
+            TeacherInfo(R.drawable.img3_6, "이호걸"),
+            TeacherInfo(R.drawable.img3_6, "박성래"),
+            TeacherInfo(R.drawable.img3_6, "유병석"),
+            TeacherInfo(R.drawable.img3_6, "이형섭"),
+            TeacherInfo(R.drawable.img3_6, "장민주")
 
         ))
 
         map.put(4, listOf(
-            TeacherInfo(R.drawable.img_3_2, "신혜정"),
-            TeacherInfo(R.drawable.img_3_7, "이대형"),
-            TeacherInfo(R.drawable.img_3_2, "이종태"),
-            TeacherInfo(R.drawable.img_4_4, "우호식"),
-            TeacherInfo(R.drawable.img_3_2, "김종성"),
-            TeacherInfo(R.drawable.img_3_7, "오수연")
+            TeacherInfo(R.drawable.img4_1, "신혜정"),
+            TeacherInfo(R.drawable.img4_2, "이대형"),
+            TeacherInfo(R.drawable.img3_6, "이종태"),
+            TeacherInfo(R.drawable.img4_4, "우호식"),
+            TeacherInfo(R.drawable.img4_5, "김종성"),
+            TeacherInfo(R.drawable.img3_6, "오수연")
         ))
 
         map.put(5, listOf(
-            TeacherInfo(R.drawable.img_3_2, "이세호"),
-            TeacherInfo(R.drawable.img_3_7, "김윤환"),
-            TeacherInfo(R.drawable.img_3_2, "권오재"),
-            TeacherInfo(R.drawable.img_3_7, "정하나")
+            TeacherInfo(R.drawable.img3_6, "이세호"),
+            TeacherInfo(R.drawable.img5_2, "김윤환"),
+            TeacherInfo(R.drawable.img3_6, "권오재"),
+            TeacherInfo(R.drawable.img3_6, "정하나")
         ))
 
 
--- app/src/main/java/com/example/e_mirim/StudentCouncilActivity.kt
+++ app/src/main/java/com/example/e_mirim/StudentCouncilActivity.kt
@@ -20,14 +20,14 @@ class StudentCouncilActivity : AppCompatActivity() {
         val dataList = listOf(
             StudentInfo(R.drawable.student, "김유진", "학생 회장"),
             StudentInfo(R.drawable.student_2, "박민지", "학생 부회장"),
-            StudentInfo(R.drawable.img, "하도연", "총무부"),
-            StudentInfo(R.drawable.img, "곽경희", "학예부"),
+            StudentInfo(R.drawable.student, "하도연", "총무부"),
+            StudentInfo(R.drawable.student, "곽경희", "학예부"),
             StudentInfo(R.drawable.student_5, "양다연", "체육부"),
             StudentInfo(R.drawable.student_6, "김소리", "생활교양부"),
-            StudentInfo(R.drawable.img, "정유진", "환경봉사부"),
-            StudentInfo(R.drawable.img, "최윤영", "훈련안전부"),
-            StudentInfo(R.drawable.img, "박선주", "서기"),
-            StudentInfo(R.drawable.img, "임채영", "회계부")
+            StudentInfo(R.drawable.student, "정유진", "환경봉사부"),
+            StudentInfo(R.drawable.student, "최윤영", "훈련안전부"),
+            StudentInfo(R.drawable.student, "박선주", "서기"),
+            StudentInfo(R.drawable.student, "임채영", "회계부")
         )
 
 
